fd8480b64a5838219fff9ac6d05652e90f5972ec
Darya <darya.slinkova@gmail.com>
2023-10-13 15:36:13 +0300

    Add BasePage and BaseTest

---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,26 @@
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.get("http://164.90.232.254/login");
+    }
+
+    @AfterMethod
+    protected void afterMethod () {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+}
